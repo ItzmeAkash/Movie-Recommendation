4eb80656821a58b92d75a68f3fea9841bea8555d
Akash Ps <Itzmeakashps@gmail.com>
2023-07-22 19:41:35 +0530

    checked similarity of the movies and recommeded five movies

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Movie-Recommendation
+# Movie-Recommendation-System
+
+## Software And Tools Requirement
+1. [Github Account](https://github.com/ItzmeAkash/Movie-Recommendation-System)
+2. [Pycharm](https://www.jetbrains.com/pycharm/)
+3. [GitCLI](https://git-scm.com/download/)
+4. [Streamlit](https://streamlit.io/)
+
+Create a New Environment
+
+```Python  -m venv env```
--- app.py
+++ app.py
@@ -2,13 +2,28 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+def recommend(movie):
+    movie_index=movies[movies['title']==movie].index[0]
+    distance=similarity[movie_index]
+    movies_list=sorted((enumerate(distance)),reverse=True,key=lambda x:x[1])[1:6]
+
+    recommended_movies=[]
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return  recommended_movies
+
+
 movies_dict=pickle.load(open('movie_dict.pkl','rb'))
 movies=pd.DataFrame(movies_dict)
-
+similarity=pickle.load(open('similarity.pkl','rb'))
 
 st.title('Movie Recommended System')
 
 
-option=st.selectbox(
+selected_movie_name=st.selectbox(
 'How would you like to be contacted',movies['title'].values
 )
+
+if st.button('Recommend'):
+    recommend(selected_movie_name)
+    st.write(selected_movie_name)
